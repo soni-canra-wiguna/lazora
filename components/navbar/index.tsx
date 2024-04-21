e6afbfa923e5d5887fe6f142f16ef43fa29224ac
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-04-21 08:42:41 +0700

    update navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -49,7 +49,7 @@ const Favourite = ({ favourites }: { favourites: number }) => {
     <div className="relative w-max h-max">
       <Heart className="size-6" strokeWidth={1.5} />
       {favourites > 0 && (
-        <span className="absolute -right-1 -bottom-1 bg-primary text-background w-4 h-4 p-2 rounded-full text-xs flex items-center justify-center">
+        <span className="absolute -right-1.5 -bottom-1 bg-primary text-background w-4 h-4 p-2 rounded-full border-[2px] border-white text-xs flex items-center justify-center">
           {favourites}
         </span>
       )}
@@ -62,7 +62,7 @@ const Cart = ({ cartItems }: { cartItems: number }) => {
     <div className="relative w-max h-max">
       <ShoppingCart className="size-6" strokeWidth={1.5} />
       {cartItems > 0 && (
-        <span className="absolute -right-1 -bottom-1 bg-primary text-background w-4 h-4 p-2 rounded-full text-xs flex items-center justify-center">
+        <span className="absolute -right-1.5 -bottom-1 bg-primary text-background w-4 h-4 p-2 rounded-full border-[2px] border-white text-xs flex items-center justify-center">
           {cartItems}
         </span>
       )}
