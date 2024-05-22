3936e5520178c182eb66f65a0d68dbe3ab614098
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-05-22 10:37:07 +0700

    feat(api): add banner api

---
+++ app/api/banners/[id]/route.ts
@@ -0,0 +1,77 @@
+import prisma from "@/lib/prismadb"
+import { NextRequest, NextResponse } from "next/server"
+import { Banner } from "@prisma/client"
+
+export const PATCH = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params
+    const body: Banner = await req.json()
+    const {
+      title,
+      description,
+      title_button,
+      href_title,
+      image,
+      alt_image,
+      background_color,
+    } = body
+
+    const updateBanner = await prisma.banner.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        title_button,
+        href_title,
+        image,
+        alt_image,
+        background_color,
+      },
+    })
+
+    if (!updateBanner) {
+      return NextResponse.json({
+        message: "banner not found, can't updated",
+        status: 404,
+      })
+    }
+
+    return NextResponse.json({
+      message: "banner successfully updated.",
+      status: 200,
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({
+      status: 500,
+      message: "Internal server error",
+    })
+  }
+}
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params
+    await prisma.banner.delete({
+      where: {
+        id,
+      },
+    })
+
+    return NextResponse.json({ status: 200, message: "banner was deleted" })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({
+      status: 500,
+      message: "Internal server error",
+    })
+  }
+}
---
+++ app/api/banners/route.ts
@@ -0,0 +1,70 @@
+import prisma from "@/lib/prismadb"
+import { Banner } from "@prisma/client"
+import { NextRequest, NextResponse } from "next/server"
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body: Banner = await req.json()
+    const {
+      title,
+      description,
+      title_button,
+      href_title,
+      image,
+      alt_image,
+      background_color,
+    } = body
+
+    await prisma.banner.create({
+      data: {
+        title,
+        description,
+        title_button,
+        href_title,
+        image,
+        alt_image,
+        background_color,
+      },
+    })
+
+    return NextResponse.json({
+      status: 201,
+      message: "banner successfully created!!",
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({
+      status: 500,
+      message: "Internal server error",
+    })
+  }
+}
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const banners = await prisma.banner.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    })
+
+    if (banners.length > 0) {
+      return NextResponse.json({
+        status: 200,
+        banners,
+        message: "Data retrieved successfully",
+      })
+    } else {
+      return NextResponse.json({
+        status: 404,
+        message: "data not found",
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({
+      status: 500,
+      message: "Internal server error",
+    })
+  }
+}
