096b78e33ebc3e48a353d849580d44025557c422
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-05-24 09:06:33 +0700

    feat(api): implement pagination in products API

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -55,7 +55,25 @@ export const POST = async (req: NextRequest) => {
 
 export const GET = async (req: NextRequest) => {
   try {
+    // pagination
+    const page = parseInt(req.nextUrl.searchParams.get("page") ?? "1")
+    const limit = parseInt(req.nextUrl.searchParams.get("limit") ?? "20")
+    const skip = (page - 1) * limit
+
+    // search product
+    const query = req.nextUrl.searchParams.get("search")
+    const decodeQuery = query?.replace(/-/g, " ")
+
+    // get total product
+    const totalProducts = await prisma.product.count()
+
     const products = await prisma.product.findMany({
+      where: {
+        title: {
+          contains: decodeQuery,
+          mode: "insensitive",
+        },
+      },
       include: {
         images: true,
         categories: true,
@@ -68,31 +86,25 @@ export const GET = async (req: NextRequest) => {
       orderBy: {
         createdAt: "desc",
       },
+      skip: skip,
+      take: limit,
     })
 
-    const query = req.nextUrl.searchParams.get("search")
-    const decodeQuery = query?.replace(/-/g, " ")
+    // when we search and result is 0
+    if (decodeQuery && products.length === 0) {
+      return NextResponse.json({
+        message: "hasil pencarian tidak di temukan",
+        status: 404,
+      })
+    }
 
-    const productsByQuery = await prisma.product.findMany({
-      where: {
-        title: {
-          contains: decodeQuery,
-          mode: "insensitive",
-        },
-      },
-      include: {
-        images: true,
-        categories: true,
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    })
-
-    if (query) {
+    if (decodeQuery) {
       return NextResponse.json({
         message: "data pencarian berhasil di ambil",
-        products: productsByQuery,
+        products,
+        currentPage: page,
+        totalPages: Math.ceil(products.length / limit),
+        totalProducts: products.length,
         status: 200,
       })
     }
@@ -101,6 +113,10 @@ export const GET = async (req: NextRequest) => {
       return NextResponse.json({
         message: "data berhasil di ambil",
         products,
+        currentPage: page,
+        totalPages: Math.ceil(totalProducts / limit),
+        totalItems: products.length,
+        totalProducts,
         status: 200,
       })
     }
@@ -110,6 +126,7 @@ export const GET = async (req: NextRequest) => {
       status: 404,
     })
   } catch (error) {
+    console.log(error)
     return NextResponse.json({
       message: "internal server error",
       error: error,
