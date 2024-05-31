f112fb6f81f97c0a22abd125dee8c602dd426ece
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-05-31 22:52:46 +0700

    style: change delete dialog
    change delete dialog from alert dialog to dialog

--- app/dashboard/banners/_components/delete-item-banner.tsx
+++ app/dashboard/banners/_components/delete-item-banner.tsx
@@ -4,19 +4,18 @@ import { Loader2, Trash2 } from "lucide-react"
 import React, { useState } from "react"
 import axios from "axios"
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { useRouter } from "next/navigation"
+  Dialog,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+  DialogContent,
+  DialogFooter,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useToast } from "@/components/ui/use-toast"
+import { Button } from "@/components/ui/button"
+import LoadingButton from "@/components/loading-button"
 
 type DeleteType = {
   id: string
@@ -24,10 +23,13 @@ type DeleteType = {
 }
 
 const DeleteItemBanner = ({ id, setIsAction }: DeleteType) => {
-  const router = useRouter()
   const { toast } = useToast()
   const queryClient = useQueryClient()
   const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const openCloseModalDialog = () => {
+    setIsOpen(!isOpen)
+  }
 
   const {
     mutate: deleteItemBanner,
@@ -43,11 +45,12 @@ const DeleteItemBanner = ({ id, setIsAction }: DeleteType) => {
         variant: "destructive",
       })
 
-      setIsOpen(false)
+      openCloseModalDialog()
       setIsAction(false)
       queryClient.invalidateQueries({ queryKey: ["banners"] })
     },
     onError: () => {
+      openCloseModalDialog()
       setIsAction(false)
       toast({
         description: "something went wrong, try again!!",
@@ -56,41 +59,42 @@ const DeleteItemBanner = ({ id, setIsAction }: DeleteType) => {
   })
 
   return (
-    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
-      <AlertDialogTrigger
-        title="delete post"
-        className="cursor-pointer"
-        asChild
-      >
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger className="cursor-pointer" asChild>
         <div className="flex justify-between items-center px-2 py-1 hover:bg-muted transition-all">
           delete
           <Trash2 size={18} strokeWidth={1.5} color="#ff0000" />
         </div>
-      </AlertDialogTrigger>
-      <AlertDialogContent className="max-w-[85vw] md:max-w-lg p-4 md:p-6">
-        <AlertDialogHeader>
-          <AlertDialogTitle>
+      </DialogTrigger>
+      <DialogContent closeIcon={false} className="max-w-[85vw] sm:max-w-lg">
+        <DialogHeader className="mb-4">
+          <DialogTitle className="font-semibold text-left leading-snug">
             Apakah anda yakin ingin menghapus banner ini?
-          </AlertDialogTitle>
-          <AlertDialogDescription>
+          </DialogTitle>
+          <DialogDescription className="text-left">
             aksi ini akan menghapus banner secara permanen dari server, apakah
             anda yakin ingin menghapusnya?
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel onClick={() => setIsAction(false)}>
-            Kembali
-          </AlertDialogCancel>
-          <AlertDialogAction
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="w-full flex sm:flex-row items-center justify-end gap-4">
+          <Button
+            className="w-full sm:w-max"
+            variant="outline"
+            onClick={openCloseModalDialog}
+          >
+            Close
+          </Button>
+          <LoadingButton
             disabled={isPending}
             onClick={() => deleteItemBanner()}
+            className="w-full sm:w-max"
           >
-            {isPending ? "loading..." : "hapus"}
+            {isPending ? "loading..." : "delete"}
             {isPending && <Loader2 className="ml-2 h-4 w-4 animate-spin" />}
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+          </LoadingButton>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
 
--- app/dashboard/products/_components/delete-item-product.tsx
+++ app/dashboard/products/_components/delete-item-product.tsx
@@ -4,19 +4,18 @@ import { Loader2, Trash2 } from "lucide-react"
 import React, { useState } from "react"
 import axios from "axios"
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { useRouter } from "next/navigation"
+  Dialog,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+  DialogContent,
+  DialogFooter,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useToast } from "@/components/ui/use-toast"
+import { Button } from "@/components/ui/button"
+import LoadingButton from "@/components/loading-button"
 
 type DeleteType = {
   id: string
@@ -24,10 +23,13 @@ type DeleteType = {
 }
 
 const DeleteItemProduct = ({ id, setIsAction }: DeleteType) => {
-  const router = useRouter()
   const { toast } = useToast()
   const queryClient = useQueryClient()
   const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const openCloseModalDialog = () => {
+    setIsOpen(!isOpen)
+  }
 
   const {
     mutate: deleteItemProduct,
@@ -43,12 +45,13 @@ const DeleteItemProduct = ({ id, setIsAction }: DeleteType) => {
         variant: "destructive",
       })
 
-      setIsOpen(false)
+      openCloseModalDialog()
       setIsAction(false)
       queryClient.invalidateQueries({ queryKey: ["products"] })
       queryClient.invalidateQueries({ queryKey: ["shuffle-products"] })
     },
     onError: () => {
+      openCloseModalDialog()
       setIsAction(false)
       toast({
         description: "something went wrong, try again!!",
@@ -57,41 +60,42 @@ const DeleteItemProduct = ({ id, setIsAction }: DeleteType) => {
   })
 
   return (
-    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
-      <AlertDialogTrigger
-        title="delete post"
-        className="cursor-pointer"
-        asChild
-      >
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild className="cursor-pointer">
         <div className="flex justify-between items-center px-2 py-1 hover:bg-muted transition-all">
           delete
           <Trash2 size={18} strokeWidth={1.5} color="#ff0000" />
         </div>
-      </AlertDialogTrigger>
-      <AlertDialogContent className="max-w-[85vw] md:max-w-lg p-4 md:p-6">
-        <AlertDialogHeader>
-          <AlertDialogTitle>
-            Apakah anda yakin ingin menghapus product ini?
-          </AlertDialogTitle>
-          <AlertDialogDescription>
-            aksi ini akan menghapus product secara permanen dari server, apakah
+      </DialogTrigger>
+      <DialogContent closeIcon={false} className="max-w-[85vw] sm:max-w-lg">
+        <DialogHeader className="mb-4">
+          <DialogTitle className="font-semibold text-left leading-snug">
+            Apakah anda yakin ingin menghapus banner ini?
+          </DialogTitle>
+          <DialogDescription className="text-left">
+            aksi ini akan menghapus banner secara permanen dari server, apakah
             anda yakin ingin menghapusnya?
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel onClick={() => setIsAction(false)}>
-            Kembali
-          </AlertDialogCancel>
-          <AlertDialogAction
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="w-full flex sm:flex-row items-center justify-end gap-4">
+          <Button
+            className="w-full sm:w-max"
+            variant="outline"
+            onClick={openCloseModalDialog}
+          >
+            kembali
+          </Button>
+          <LoadingButton
             disabled={isPending}
             onClick={() => deleteItemProduct()}
+            className="w-full sm:w-max"
           >
             {isPending ? "loading..." : "hapus"}
             {isPending && <Loader2 className="ml-2 h-4 w-4 animate-spin" />}
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+          </LoadingButton>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
 
