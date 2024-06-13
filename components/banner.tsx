cf404e2a01f09d85af2fc4874153ec248b981cbd
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-06-13 22:30:48 +0700

    style: update loading and error banner UI

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,8 +1,5 @@
 "use client"
 
-import Image from "next/image"
-import { Button } from "./ui/button"
-import { Card } from "./ui/card"
 import {
   Carousel,
   CarouselContent,
@@ -11,62 +8,61 @@ import {
   CarouselPrevious,
 } from "./ui/carousel"
 import AutoPlay from "embla-carousel-autoplay"
-import test from "@/public/moi.png"
-import Balancer from "react-wrap-balancer"
-import { ArrowRight, Coffee } from "lucide-react"
-import Link from "next/link"
 import { getBanners } from "@/services/get-banners"
 import { Skeleton } from "./ui/skeleton"
 import BannerItem from "./banner-item"
+import MaxWidthWrapper from "./max-width-wrapper"
 
 const BannerHomepage = () => {
   const { data, isPending, isError } = getBanners()
 
   return (
-    <section className="w-full mb-20 group">
-      <Carousel
-        plugins={[
-          AutoPlay({
-            delay: 7000,
-          }),
-        ]}
-        opts={{ loop: true }}
-      >
-        <CarouselContent>
-          {isPending ? (
-            <CarouselItem>
-              <LoadingBanner />
-            </CarouselItem>
-          ) : isError ? (
-            <CarouselItem>
-              <Skeleton className="w-full h-[450px] border-none rounded-xl flex items-center justify-center overflow-hidden bg-secondary">
-                <p className="font-medium text-lg">
-                  Ada yang salah nih, coba deh refresh lagi
-                </p>
-              </Skeleton>
-            </CarouselItem>
-          ) : (
-            data?.map((banner) => (
-              <CarouselItem key={banner.title}>
-                <BannerItem {...banner} />
+    <MaxWidthWrapper>
+      <section className="group mb-20 w-full">
+        <Carousel
+          plugins={[
+            AutoPlay({
+              delay: 7000,
+            }),
+          ]}
+          opts={{ loop: true }}
+        >
+          <CarouselContent>
+            {isPending ? (
+              <CarouselItem>
+                <LoadingBanner />
               </CarouselItem>
-            ))
+            ) : isError ? (
+              <CarouselItem>
+                <Skeleton className="flex h-[450px] w-full items-center justify-center overflow-hidden rounded-xl border-none bg-secondary">
+                  <p className="text-lg font-medium">
+                    Ada yang salah nih, coba deh refresh lagi
+                  </p>
+                </Skeleton>
+              </CarouselItem>
+            ) : (
+              data?.map((banner) => (
+                <CarouselItem key={banner.title}>
+                  <BannerItem {...banner} />
+                </CarouselItem>
+              ))
+            )}
+          </CarouselContent>
+          {isPending ? null : (
+            <>
+              <CarouselPrevious
+                className="left-8 size-12 border-none bg-background opacity-0 shadow-md transition-all duration-300 hover:border-none hover:shadow-lg group-hover:-left-6 group-hover:opacity-100"
+                classNameIcon="size-6 stroke-[1.5]"
+              />
+              <CarouselNext
+                className="right-8 size-12 border-none bg-background opacity-0 shadow-md transition-all duration-300 hover:border-none hover:shadow-lg group-hover:-right-6 group-hover:opacity-100"
+                classNameIcon="size-6 stroke-[1.5]"
+              />
+            </>
           )}
-        </CarouselContent>
-        {isPending ? null : (
-          <>
-            <CarouselPrevious
-              className="left-8 size-12 bg-background opacity-0 group-hover:opacity-100 group-hover:-left-6 transition-all duration-300 border-none hover:border-none shadow-md hover:shadow-lg"
-              classNameIcon="size-6 stroke-[1.5]"
-            />
-            <CarouselNext
-              className="right-8 size-12 bg-background opacity-0 group-hover:opacity-100 group-hover:-right-6 transition-all duration-300 border-none hover:border-none shadow-md hover:shadow-lg"
-              classNameIcon="size-6 stroke-[1.5]"
-            />
-          </>
-        )}
-      </Carousel>
-    </section>
+        </Carousel>
+      </section>
+    </MaxWidthWrapper>
   )
 }
 
@@ -74,19 +70,19 @@ export default BannerHomepage
 
 const LoadingBanner = () => {
   return (
-    <Skeleton className="w-full h-[450px] border-none rounded-xl grid grid-cols-12 overflow-hidden bg-secondary">
+    <Skeleton className="grid h-[450px] w-full grid-cols-12 overflow-hidden rounded-xl border-none bg-secondary">
       <div className="col-span-7 flex items-center pl-20">
         <div className="flex flex-col">
-          <div className="flex items-center gap-3 mb-4 w-full h-max">
-            <Skeleton className="w-[80px] h-8 rounded-full bg-secondary-foreground/10" />
-            <Skeleton className="w-[300px] h-8 rounded-full bg-secondary-foreground/10" />
+          <div className="mb-4 flex h-max w-full items-center gap-3">
+            <Skeleton className="h-8 w-[80px] rounded-full bg-secondary-foreground/10" />
+            <Skeleton className="h-8 w-[300px] rounded-full bg-secondary-foreground/10" />
           </div>
-          <Skeleton className="w-[250px] h-3 rounded-full bg-secondary-foreground/10 mb-12" />
-          <Skeleton className="w-32 h-11 rounded-full bg-secondary-foreground/10" />
+          <Skeleton className="mb-12 h-3 w-[250px] rounded-full bg-secondary-foreground/10" />
+          <Skeleton className="h-11 w-32 rounded-full bg-secondary-foreground/10" />
         </div>
       </div>
       <div className="col-span-5 flex items-center justify-center">
-        <Skeleton className="size-[90%] rounded-2xl overflow-hidden flex items-center justify-center bg-secondary-foreground/10">
+        <Skeleton className="flex size-[90%] items-center justify-center overflow-hidden rounded-2xl bg-secondary-foreground/10">
           {/* <Skeleton className="size-1/2 bg-secondary-foreground/20" /> */}
         </Skeleton>
       </div>
