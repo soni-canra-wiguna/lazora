969b463789628746041655aca9f268c2d9d9dbca
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-06-15 13:04:52 +0700

    chore: add max width wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import { useUserServer } from "@/hook/use-user"
 import ListProducts from "@/components/products"
-import MaxWidthWrapper from "@/components/max-width-wrapper"
 import BannerHomepage from "@/components/banner"
+import PreviewKeyboards from "@/components/preview-keyboard"
 
 export default async function Home() {
   const session = await useUserServer()
   return (
-    <MaxWidthWrapper className="min-h-screen py-32">
+    <div className="min-h-screen py-32">
       <BannerHomepage />
       <ListProducts />
-    </MaxWidthWrapper>
+      {/* <PreviewKeyboards /> */}
+    </div>
   )
 }
--- components/products.tsx
+++ components/products.tsx
@@ -5,41 +5,44 @@ import { ProductCard2 } from "./product-card"
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
 import { Button } from "./ui/button"
 import { ChevronDown } from "lucide-react"
+import MaxWidthWrapper from "./max-width-wrapper"
 
 const ListProducts = () => {
   const { data, isPending, isError } = getShuffleProducts()
   return (
-    <div className="flex flex-col gap-10">
-      <div className="flex items-center justify-between">
-        <h3 className="text-2xl font-medium">Products</h3>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button variant="outline" className="rounded-full">
-              Sort by <ChevronDown className="size-4 ml-2" />{" "}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent align="end" className="rounded-none shadow-none">
-            price
-          </PopoverContent>
-        </Popover>
+    <MaxWidthWrapper>
+      <div className="flex flex-col gap-10">
+        <div className="flex items-center justify-between">
+          <h3 className="text-2xl font-medium">Products</h3>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="outline" className="rounded-full">
+                Sort by <ChevronDown className="size-4 ml-2" />{" "}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent align="end" className="rounded-none shadow-none">
+              price
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+          {isPending
+            ? "loading data..."
+            : isError
+            ? "productnya kenapa nih"
+            : data?.map(({ id, images, title, categories, price }) => (
+                <ProductCard2
+                  key={id + title}
+                  id={id}
+                  image={images[0]}
+                  title={title}
+                  categories={categories}
+                  price={price}
+                />
+              ))}
+        </div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
-        {isPending
-          ? "loading data..."
-          : isError
-          ? "productnya kenapa nih"
-          : data?.map(({ id, images, title, categories, price }) => (
-              <ProductCard2
-                key={id + title}
-                id={id}
-                image={images[0]}
-                title={title}
-                categories={categories}
-                price={price}
-              />
-            ))}
-      </div>
-    </div>
+    </MaxWidthWrapper>
   )
 }
 
