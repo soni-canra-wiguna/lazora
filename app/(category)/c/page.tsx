5cad387a8e3ae57ab74a4b1e0d7097c94baae933
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-06-20 11:27:28 +0700

    WIP category page

---
+++ app/(category)/c/page.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/max-width-wrapper"
+
+const CategoryPage = () => {
+  return (
+    <MaxWidthWrapper className="mt-32 min-h-screen">
+      <div>category page</div>
+    </MaxWidthWrapper>
+  )
+}
+
+export default CategoryPage
