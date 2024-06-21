433e4001320ea109cbdf71e33fff0155deab1063
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-06-21 16:21:41 +0700

    chore:  improve UI
    improve not found page & denied page UI

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,6 +2,7 @@ import BackButton from "@/components/back-button"
 import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
 import { Metadata } from "next"
+import Image from "next/image"
 import Link from "next/link"
 import React from "react"
 
@@ -11,15 +12,21 @@ export const metadata: Metadata = {
 
 const NotFound = () => {
   return (
-    <div className="w-screen h-screen top-0 left-0 flex items-center justify-center overflow-hidden z-50 fixed bg-background">
-      <div className="max-w-lg w-full mx-auto flex flex-col gap-8 items-center px-6 justify-center">
-        <h1 className="text-4xl font-grostekBold">page not found</h1>
+    <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center overflow-hidden bg-background">
+      <div className="mx-auto flex w-full max-w-lg flex-col items-center justify-center gap-8 px-6">
+        <Image
+          alt="image not found"
+          src="/notfound.svg"
+          width={500}
+          height={500}
+          className="aspect-square w-full max-w-sm grayscale selection:bg-transparent"
+        />
         <div className="flex items-center gap-4">
           <BackButton />
           <Button variant="outline" asChild className="group">
             <Link href="/">
               ke halaman utama
-              <ArrowRight className="ml-2 size-4 group-hover:translate-x-1 transition-all" />
+              <ArrowRight className="ml-2 size-4 transition-all group-hover:translate-x-1" />
             </Link>
           </Button>
         </div>
