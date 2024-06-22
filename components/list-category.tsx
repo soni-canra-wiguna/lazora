e5d559c44306684aa098ce398a8a4b632a8eacf8
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-06-22 10:01:07 +0700

    style: update category list UI

--- components/list-category.tsx
+++ components/list-category.tsx
@@ -1,22 +1,35 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./max-width-wrapper"
 import { CATEGORIES } from "@/data/categories"
-import { Button } from "./ui/button"
+import { Card } from "./ui/card"
+import Image from "next/image"
 
 const ListCategory = () => {
   return (
-    <MaxWidthWrapper className="scrollX my-10 flex items-center gap-5 overflow-x-auto pb-2">
+    <MaxWidthWrapper className="mb-20 flex flex-row flex-wrap gap-4">
       {CATEGORIES.map((c, index) => {
         const replaceCategory = c.value.toLowerCase().replace(/ /g, "-") // from "hello world" to "hello-world"
         const url = `/c/${replaceCategory}`
         return (
-          <Link href={url} className="size-max" key={index}>
-            <Button
-              className="m-max rounded-full capitalize"
-              variant="secondary"
-            >
-              {c.title}
-            </Button>
+          <Link
+            href={url}
+            className="group inline-block overflow-hidden"
+            key={index}
+          >
+            <Card className="relative flex aspect-[14/8] w-[260px] items-center justify-center rounded-xl border-none bg-secondary transition-all duration-300 hover:bg-secondary-foreground/10">
+              <h3 className="z-20 font-canelaLight capitalize tracking-wide transition-all duration-300 group-hover:-translate-x-16">
+                {c.title}
+              </h3>
+              <div className="absolute right-0 top-1/2 aspect-square w-[200px] -translate-y-1/2 translate-x-52 transition-all duration-300 group-hover:translate-x-14">
+                <Image
+                  width={500}
+                  height={500}
+                  alt={c.title}
+                  src={c.image}
+                  className="size-full object-center grayscale"
+                />
+              </div>
+            </Card>
           </Link>
         )
       })}
--- data/categories.ts
+++ data/categories.ts
@@ -2,33 +2,41 @@ export const CATEGORIES = [
   {
     title: "keyboard",
     value: "keyboard",
+    image: "/categories/keyboard.png",
   },
   {
     title: "deskmat",
     value: "deskmat",
+    image: "/categories/deskmat.png",
   },
   {
     title: "keycaps",
     value: "keycaps",
+    image: "/categories/keycaps.png",
   },
   {
     title: "coiled cable",
     value: "coiled cable",
+    image: "/categories/coiled-cable.png",
   },
   {
     title: "mouse",
     value: "mouse",
+    image: "/categories/mouse.png",
   },
   {
     title: "switch",
     value: "switch",
+    image: "/categories/switch.png",
   },
   {
     title: "sticker",
     value: "sticker",
+    image: "/categories/sticker.png",
   },
   {
     title: "barebone",
     value: "barebone",
+    image: "/categories/barebone.png",
   },
 ]
