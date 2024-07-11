a9e0e16383a36cdfac156d3b44ed3b5ccf6faa39
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-07-11 21:08:55 +0700

    feat: improve image single product
    - add modal image slider
    - change slide image navigation from router to state

--- components/detail-product/image-product.tsx
+++ components/detail-product/image-product.tsx
@@ -3,57 +3,127 @@
 import { ImageProps } from "@/types"
 import Image from "next/image"
 import { useRouter, useSearchParams } from "next/navigation"
+import * as DialogPrimitive from "@radix-ui/react-dialog"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { useState } from "react"
+import { X } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 interface ImageProductProps {
   images: ImageProps[]
   title: string
 }
 
-const ImageProduct = ({ images, title }: ImageProductProps) => {
-  const router = useRouter()
-  const searchParam = useSearchParams()
-  const param: any = searchParam.get("indexImg" || null)
+export default function ImageProduct({ images, title }: ImageProductProps) {
+  const [indexImage, setIndexImage] = useState(0)
+  // const router = useRouter()
+  // const searchParam = useSearchParams()
+  // const param: any = searchParam.get("indexImg" || null)
 
-  const handleTabsImage = (index: number) => {
-    router.prefetch(`?indexImg=${index}`)
-    router.push(`?indexImg=${index}`)
-  }
+  // const handleTabsImage = (index: number) => {
+  //   router.prefetch(`?indexImg=${index}`)
+  //   router.push(`?indexImg=${index}`)
+  // }
 
   return (
-    <div className="flex-1 sticky top-32 flex gap-4 items-start w-full h-[620px]">
-      <div className="w-[60px] grid grid-cols-1 gap-4">
+    <div className="sticky top-32 flex h-[620px] w-full flex-1 items-start gap-4">
+      <div className="grid w-[60px] grid-cols-1 gap-4">
         {images.map(({ image }, index) => (
           <div
             key={index}
-            onClick={() => handleTabsImage(index)}
-            className={`${
-              index == (param || 0) &&
-              "hover:brightness-75 brightness-75 border-primary"
-            } transition-all hover:brightness-75 cursor-pointer w-full aspect-square bg-secondary border border-border/40`}
+            onClick={() => setIndexImage(index)}
+            className={cn(
+              "aspect-square w-full cursor-pointer border border-border/40 bg-secondary transition-all selection:bg-transparent hover:brightness-75",
+              index == (indexImage || 0) &&
+                "border-primary brightness-75 hover:brightness-75",
+            )}
           >
             <Image
               src={image ?? ""}
               alt={title}
               width={250}
               height={250}
-              // placeholder="blur"
               className="size-full object-cover object-center"
             />
           </div>
         ))}
       </div>
-      <div className="size-full flex-1">
-        <Image
-          src={images[param || 0].image ?? ""}
-          alt={title}
-          width={700}
-          height={700}
-          // placeholder="blur"
-          className="size-full object-cover object-center"
-        />
-      </div>
+      <ModalImageSlider images={images} title={title} indexImage={indexImage} />
     </div>
   )
 }
 
-export default ImageProduct
+const DialogClose = DialogPrimitive.Close
+
+type ModalImageSliderType = ImageProductProps & {
+  indexImage: number
+}
+
+const ModalImageSlider = ({
+  images,
+  title,
+  indexImage,
+}: ModalImageSliderType) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="relative size-full flex-1 cursor-zoom-in selection:bg-transparent">
+          <Image
+            src={images[indexImage || 0].image ?? ""}
+            alt={title}
+            fill
+            className="object-cover object-center"
+          />
+        </div>
+      </DialogTrigger>
+      <DialogContent
+        className="max-w-screen h-screen w-screen overflow-hidden p-0"
+        closeIcon={false}
+      >
+        <Carousel
+          opts={{
+            loop: true,
+          }}
+          className="mx-auto h-full w-full max-w-[80%]"
+        >
+          <CarouselContent classNameParent="h-full">
+            {images.map((image) => (
+              <CarouselItem
+                key={image.id}
+                className="relative flex h-screen w-full items-center selection:bg-transparent"
+              >
+                <Image
+                  alt={title}
+                  src={image.image ?? ""}
+                  fill
+                  className="object-contain"
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {/* button slide */}
+          <div className="absolute bottom-10 left-1/2 flex -translate-x-1/2 items-center gap-5">
+            <CarouselPrevious
+              className="relative -left-0 flex size-12 transform-none items-center justify-center rounded-full border-none bg-background shadow-md hover:border-none"
+              classNameIcon="size-6"
+            />
+            <DialogClose className="group flex size-12 items-center justify-center rounded-full bg-background shadow-md">
+              <X className="size-6 text-primary transition-all duration-300 group-hover:text-red-500" />
+            </DialogClose>
+            <CarouselNext
+              className="group relative -right-0 flex size-12 transform-none items-center justify-center rounded-full border-none bg-background shadow-md hover:border-none"
+              classNameIcon="size-6"
+            />
+          </div>
+        </Carousel>
+      </DialogContent>
+    </Dialog>
+  )
+}
