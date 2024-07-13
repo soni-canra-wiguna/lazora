428fe394edad4f5e0924f3663e709e3f96ff3ba6
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-07-14 02:32:39 +0700

    fix: fix banner viewer

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,17 @@
+"use client"
+
+import { useUserClient } from "@/hook/use-user"
 import Image from "next/image"
 
 const DashboardPage = () => {
+  const { session } = useUserClient()
   return (
     <div className="flex w-full flex-col">
-      <div className="w-full border border-main/30 bg-main/10 px-6 py-4 font-medium text-main">
-        Kamu seorang viewer, kamu tidak bisa melakukan aksi apapun!!
-      </div>
+      {session?.user.role === "VIEWER" && (
+        <div className="w-full border border-main/30 bg-main/10 px-6 py-4 font-medium text-main">
+          Kamu seorang viewer, kamu tidak bisa melakukan aksi apapun!!
+        </div>
+      )}
       <div className="mx-auto mt-24 w-full max-w-lg flex-col items-center gap-8 px-6">
         <Image
           alt="image not found"
