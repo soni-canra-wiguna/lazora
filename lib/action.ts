41e90a4e70a2e6991a5838ea42d56a3d5e6999fd
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-07-17 10:50:06 +0700

    refactor: remove unnecessary code

--- lib/action.ts
+++ lib/action.ts
@@ -1,43 +1,9 @@
 "use server"
 
-import * as z from "zod"
 import prisma from "./prismadb"
 import { revalidateTag } from "next/cache"
 import { CommentSchema } from "@/schema"
 import { createSafeActionClient } from "next-safe-action"
-
-type PostComment = z.infer<typeof CommentSchema>
-
-// export async function postComment({
-//   comment,
-//   username,
-//   email,
-//   image,
-//   role,
-//   productId,
-// }: PostComment) {
-//   try {
-//     await prisma.comment.create({
-//       data: {
-//         // @ts-ignore
-//         message: comment,
-//         username,
-//         email,
-//         image,
-//         role,
-//         product: {
-//           connect: {
-//             id: productId,
-//           },
-//         },
-//       },
-//     })
-
-//     revalidateTag("singleProduct")
-//   } catch (error) {
-//     return { message: "failed to post comment" }
-//   }
-// }
 
 export const action = createSafeActionClient()
 
@@ -64,7 +30,8 @@ export const createSafeComment = action(
 
       revalidateTag("singleProduct")
     } catch (error) {
+      console.log(error)
       return { message: "failed to post comment" }
     }
-  }
+  },
 )
