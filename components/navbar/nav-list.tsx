47801a3e283c34783924e17c43194aa4e71012b0
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-07-24 20:07:20 +0700

    improve navlink categories

--- components/navbar/nav-list.tsx
+++ components/navbar/nav-list.tsx
@@ -1,28 +1,62 @@
 import Link from "next/link"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu"
+import React from "react"
+import { CATEGORIES } from "@/constants/categories"
+import { formatTitleProduct } from "@/utils/format-title-product"
 
 const NavList = () => {
   return (
-    <div className="mt-1 flex items-center gap-7">
+    <div className="mt-1 flex items-center gap-8">
       <Link
-        className="text-sm font-medium capitalize leading-none text-secondary-foreground/70 hover:text-primary"
+        className="text-sm font-medium capitalize leading-none text-primary hover:text-secondary-foreground/70"
         href="/p?sortBy=featured"
       >
-        all products
+        products
       </Link>
+      <CategoryMenu />
       <Link
-        className="text-sm font-medium capitalize leading-none text-secondary-foreground/70 hover:text-primary"
-        href="/c"
+        className="text-sm font-medium capitalize leading-none text-primary hover:text-secondary-foreground/70"
+        href="/blog"
       >
-        category
-      </Link>
-      <Link
-        className="text-sm font-medium capitalize leading-none text-secondary-foreground/70 hover:text-primary"
-        href="/"
-      >
-        popular
+        Blog
       </Link>
     </div>
   )
 }
 
 export default NavList
+
+const CategoryMenu = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="px-0 text-primary hover:bg-transparent hover:text-secondary-foreground/70">
+            categories
+          </NavigationMenuTrigger>
+          <NavigationMenuContent className="rounded-none px-0 py-2 md:rounded-none">
+            <ul className="flex h-max w-60 flex-col space-y-2.5">
+              {CATEGORIES.map((category) => (
+                <li className="p-2 px-6 hover:bg-secondary">
+                  <Link
+                    href={`/c/${formatTitleProduct(category.value)}`}
+                    className="flex w-full items-center gap-2.5 text-sm capitalize"
+                  >
+                    {category.icon}
+                    {category.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
--- constants/categories.tsx
+++ constants/categories.tsx
@@ -1,42 +1,52 @@
+import {Box, Cable, CircuitBoard, Container, Keyboard, Mouse, Proportions, Sticker} from "lucide-react"
+
 export const CATEGORIES = [
   {
     title: "keyboard",
     value: "keyboard",
     image: "/categories/keyboard.png",
+    icon: <Keyboard className="size-4" />,
   },
   {
     title: "deskmat",
     value: "deskmat",
     image: "/categories/deskmat.png",
+    icon: <Proportions className="size-4" />,
   },
   {
     title: "keycaps",
     value: "keycaps",
     image: "/categories/keycaps.png",
+    icon: <Box className="size-4" />,
   },
   {
     title: "coiled cable",
     value: "coiled cable",
     image: "/categories/coiled-cable.png",
+    icon: <Cable className="size-4" />,
   },
   {
     title: "mouse",
     value: "mouse",
     image: "/categories/mouse.png",
+    icon: <Mouse className="size-4" />,
   },
   {
     title: "switch",
     value: "switch",
     image: "/categories/switch.png",
+    icon: <Container className="size-4" />,
   },
   {
     title: "sticker",
     value: "sticker",
     image: "/categories/sticker.png",
+    icon: <Sticker className="size-4" />,
   },
   {
     title: "barebone",
     value: "barebone",
     image: "/categories/barebone.png",
+    icon: <CircuitBoard  className="size-4" />,
   },
 ]
