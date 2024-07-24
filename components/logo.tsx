371d3f5f4180576395b2921dfc4ad314b793aa9b
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-07-24 20:07:35 +0700

    improve logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,15 +1,17 @@
 import { cn } from "@/lib/utils"
 import Image from "next/image"
 import Link from "next/link"
-import lazora from "@/public/lazora.svg"
+import lazora from "@/public/lazora.png"
 
 const Logo = ({ className }: { className?: string }) => {
   return (
-    <Link href="/" className={cn("flex items-center gap-3", className)}>
-      {/* <Image alt="lazora logo" src={lazora} className="size-6" /> */}
-      <h1 className="text-2xl font-canelaRegular text-inherit uppercase">
-        Lazora
-      </h1>
+    <Link href="/" className={cn("group flex items-center gap-3", className)}>
+      <Image
+        alt="lazora logo"
+        src={lazora}
+        className="size-6 transition-all duration-300 group-hover:scale-105"
+      />
+      <h1 className="text-2xl font-semibold uppercase text-inherit">Lazora</h1>
     </Link>
   )
 }
